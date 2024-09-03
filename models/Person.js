26280ae71b05ee68263252d3c02ddc5a1fa31f42
kayode <olowookerekayode6@gmail.com>
2024-09-03 12:22:12 +0100

    first commit

---
+++ models/Person.js
@@ -0,0 +1,14 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// // Define the Person schema
+// const personSchema = new Schema({
+//   name: { type: String, required: true },
+//   age: { type: Number },
+//   favoriteFoods: { type: [String] }
+// });
+
+// // Create the Person model
+// const Person = mongoose.model('Person', personSchema);
+
+// module.exports = Person;
